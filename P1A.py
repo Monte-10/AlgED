917a9b570a59a00d76183a72cbf08fa58f945d4a
Monte-10 <61688940+Monte-10@users.noreply.github.com>
2022-09-12 15:34:35 +0200

    Ejercicio 1 A código terminado

--- P1A.py
+++ P1A.py
@@ -18,4 +18,11 @@ def matrix_multiplication(m_1: np.ndarray, m_2: np.ndarray) -> np.ndarray :
 m1 = np.array([[1,2,3],[4,5,6], [7,8,9]])
 m2 = np.array([[9,8,7],[6,5,4],[3,2,1]])
 
-print(matrix_multiplication(m1, m2))
+l_timings = []
+for i in range(11):
+    dim = 10+i
+    m = np.random.uniform(0., 1., (dim, dim))
+    timings = %timeit -o -n 10 -r 5 -q matrix_multiplication(m1, m2)
+    l_timings.append([dim, timings.best])
+
+print(l_timings)
