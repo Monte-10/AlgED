087b3eb90fb816ba878d4adf86f86df8af44e964
Monte-10 <61688940+Monte-10@users.noreply.github.com>
2022-09-12 17:47:24 +0200

    Práctica 1 ejercicio B terminado

--- P1B.py
+++ P1B.py
@@ -2,18 +2,43 @@ import numpy as np
 
 def rec_bb(t: list, f: int, l: int, key: int) -> int :
     
-    mid = (f+l) // 2
+    if l>=f:
 
-    if t[mid] == key:
-        return mid
-    
-    elif t[mid] < key:
-        return rec_bb(t, mid+1, l, key)
+        mid = (f+l) // 2
 
-    elif t[mid > key]:
-        return rec_bb(t, f, mid-1, key)
+        if t[mid] == key:
+            return mid
+        
+        elif t[mid] < key:
+            return rec_bb(t, mid+1, l, key)
+
+        else:
+            return rec_bb(t, f, mid-1, key)
 
     else:
         return None
 
-print(rec_bb((1,2,3,4,5,6,7,8,9,10),1,10,7))
+def bb(t: list, f: int, l: int, key: int) -> int :
+
+    while f <= l:
+
+        mid = (f+l) // 2
+
+        if t[mid] == key:
+            return mid
+
+        elif t[mid] < key:
+            f = mid + 1
+
+        elif t[mid] > key:
+            l = mid -1
+    
+    return None
+
+l_times = []
+for i, size in enumerate(range(5, 15)):
+    t = list(range(2**i * size))
+    key = 8
+    timings = %timeit -n 100 -r 10 -o -q rec_bb(t, 0, len(t) - 1, key)
+    l_times.append([len(t), timings.best])
+    times = np.array(l_times)
